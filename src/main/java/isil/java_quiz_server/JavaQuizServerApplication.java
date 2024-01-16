7d850f4d3c025433ad0f4bfbd6ab9b215db20521
MehKad <mehkadiri@gmail.com>
2024-01-16 22:45:06 +0000

    init file server

---
+++ src/main/java/isil/java_quiz_server/JavaQuizServerApplication.java
@@ -0,0 +1,13 @@
+package isil.java_quiz_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQuizServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaQuizServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isil/java_quiz_server/JavaQuizServerApplicationTests.java
@@ -0,0 +1,13 @@
+package isil.java_quiz_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQuizServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
