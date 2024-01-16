02964941fdbbee7dccda5304810bccdf4153679c
MehKad <mehkadiri@gmail.com>
2024-01-16 23:02:07 +0000

    user database

---
+++ src/main/java/isil/java_quiz_server/modal/User.java
@@ -0,0 +1,63 @@
+package isil.java_quiz_server.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username, password, email;
+    private Number phone;
+    private Boolean is_teacher;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Number getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Number phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getIs_teacher() {
+        return is_teacher;
+    }
+
+    public void setIs_teacher(Boolean is_teacher) {
+        this.is_teacher = is_teacher;
+    }
+}
---
+++ src/main/java/isil/java_quiz_server/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package isil.java_quiz_server.repository;
+
+import isil.java_quiz_server.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
