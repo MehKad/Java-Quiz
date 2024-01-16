abdc9af1e88aaedf6175dfef37a6820b591dd997
MehKad <mehkadiri@gmail.com>
2024-01-16 23:41:46 +0000

    List all users

--- src/main/java/isil/java_quiz_server/controller/UserController.java
+++ src/main/java/isil/java_quiz_server/controller/UserController.java
@@ -4,9 +4,12 @@ package isil.java_quiz_server.controller;
 import isil.java_quiz_server.modal.User;
 import isil.java_quiz_server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,6 +19,11 @@ public class UserController {
     @PostMapping("/user")
     User newUser( @RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
 }
